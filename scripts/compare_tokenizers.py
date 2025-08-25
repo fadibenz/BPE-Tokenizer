3c28f96095f84f5dde5dea5cce12cee7caf17830
fadi benzaima <f.benzaima@esi-sba.dz>
2025-08-25 11:55:47 +0300

    refactor: Fix logs and CLI arguments description

--- scripts/compare_tokenizers.py
+++ scripts/compare_tokenizers.py
@@ -5,11 +5,11 @@ from Experiments.data_leakage_experiment.data_leakage import data_leakage_experi
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Running experiment for data leakage")
+    parser = argparse.ArgumentParser(description="Running comparison between tokenizers")
     parser.add_argument("--tokenizer_a", type=str, required=True,
-                        help="Path to vocab.json and merges.txt for tokenizer without leak")
+                        help="Path to vocab.json and merges.txt for first tokenizer")
     parser.add_argument("--tokenizer_b", type=str, required=True,
-                        help="Path to vocab.json and merges.txt for tokenizer with leak")
+                        help="Path to vocab.json and merges.txt for second tokenizer")
     parser.add_argument("--eval_path", type=str, required=True, help="Path to corpus")
     parser.add_argument("--samples", type=int, default=1000, help="Number of samples")
 
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         Path(args.tokenizer_b) / "train_bpe_merges.txt",
         special_tokens=special_tokens
     )
-    print("\n Running data leakage experiment")
+    print("\n Running Comparison")
 
     data_leakage_experiment(tokenizer_a, tokenizer_b,
                             path_corpus / "valid.txt", path_corpus / "test.txt",
